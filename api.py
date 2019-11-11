7080d9fa5a6043de3dc74fd2ebc01b92e5e21a94
Stănilă Ioan <stanila.ionutsss@gmail.com>
2019-11-11 23:36:30 +0100

    Add files via upload

--- api.py
+++ api.py
@@ -1,81 +1,51 @@
-import falcon
-import mimetypes
 import os
-import io
-import base64
-import json
-from falcon_cors import CORS
-from waitress import serve
+from flask import Flask, request, make_response, jsonify, abort
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 
-# class Depth:
-#     def on_post(self, req, resp):
-#         alg = req.media["alg"]
-#         if int(alg) in algs:
-#             base64_image = req.media["image"]
-#             try:
-#                 image = Image.open(io.BytesIO(base64.b64decode(base64_image)))
-#                 depth = predict_depth(alg, image)["depth"]
-#                 output = io.BytesIO()
-#                 depth.save(output, format="PNG")
-#                 im_data = output.getvalue()
-#                 depth_map = "data:image/png;base64," + base64.b64encode(im_data).decode("utf-8")
-#                 resp.status = falcon.HTTP_200
-#                 resp.body = json.dumps({"image": depth_map})
-#             except:
-#                 resp.status = falcon.HTTP_400
-#                 resp.body = json.dumps({"error": "invalid image data"})
-#         else:
-#             resp.status = falcon.HTTP_400
-#             resp.body = json.dumps({"error": "invalid algorithm"})
+root_directory = "./objects/"
 
 
-# class DepthInfo:
-#     def on_post(self, req, resp):
-#         alg = req.media["alg"]
-#         if int(alg) in algs:
-#             base64_image = req.media["image"]
-#             try:
-#                 image = Image.open(io.BytesIO(base64.b64decode(base64_image)))
-#                 result = predict_depth_info(alg, image)
-#                 depth = result["depth"]
-#                 resp.body = json.dumps({"image": depth.tolist(), "time": result["time"]})
-#             except:
-#                 resp.status = falcon.HTTP_400
-#                 resp.body = json.dumps({"error": "invalid image data"})
-#         else:
-#             resp.status = falcon.HTTP_400
-#             resp.body = json.dumps({"error": "invalid algorithm"})
+app = Flask(__name__)
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["2 per minute", "1 per second"],
+)
 
-class Index(object):
-    def on_get(self, req, resp):
-        resp.status = falcon.HTTP_200
-        resp.content_type = 'text/html'
-        with open("static/index.html", 'r') as f:
-            resp.body = f.read()
+@app.route('/', methods=['GET'])
+def get_all():
+    return make_response(jsonify(os.listdir(root_directory)), 200)
 
-class StaticResource(object):
-    def on_get(self, req, resp, type, filename):
-        # do some sanity check on the filename
-        resp.status = falcon.HTTP_200
-        
-        resp.content_type = ""
-        with open("static/" + filename, 'r') as f:
-            resp.body = f.read()
+@app.route('/', methods=['DELETE'])
+@limiter.limit("1 per hour")
+def delete_all():
+    for file in os.listdir(root_directory):
+        os.remove(root_directory + file)
+    return '', 200
 
-class Remix(object):
-    def on_post(self, req, resp, type):
-        resp.status = falcon.HTTP_200
-        resp.body = json.dumps({"filename": "bensound-summer.mp3"})
+@app.route('/objs/<string:obj_id>', methods=['GET'])
+def retieve(obj_id):
+    if obj_id in os.listdir(root_directory):
+        with open(root_directory + obj_id, 'rb') as f:
+            return make_response(f.read(), 200)
+    else:
+        abort(404)
 
+@app.route('/objs/<string:obj_id>', methods=['PUT'])
+def store(obj_id):
+    with open(root_directory + obj_id, 'wb') as f:
+        f.write(request.data)
+    return '', 200
 
-# allow_origins_list=['http://localhost:5000']
-cors = CORS(allow_all_origins=True, allow_all_headers=True, allow_all_methods=True)
+@app.route('/objs/<string:obj_id>', methods=['DELETE'])
+def remove(obj_id):
+    if obj_id in os.listdir(root_directory):
+        os.remove(root_directory + obj_id)
+        return '', 200
+    else:
+        abort(404)
 
-api = application = falcon.API(middleware=[cors.middleware])
-
-api.add_route('/', Index())
-api.add_route('/{filename}', StaticResource())
-api.add_route('/remix/{type}', Remix())
-
-serve(api, host="127.0.0.1", port=8000)
+if __name__ == '__main__':
+    app.run(debug=True)
