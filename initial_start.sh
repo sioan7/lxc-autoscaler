e0abf814d2fd5ea3ff4d44b180361ee706ffbd8d
illihcs <j.schillemans@gmx.de>
2019-11-17 09:05:24 -0500

    organized initial startup shell script

--- initial_start.sh
+++ initial_start.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
-#echo 'Start webservice'
-#lxc-copy -e -B overlay -n webapp
+
+echo 'First webapp will be started by the scalingcontroller!'
 
 echo 'Start loadbalancer'
 lxc-start -n loadbalancer
 
 echo 'Start scalingcontroller'
 python3 ./scalingcontroller/scalingcontroller.py
-
-# The scalingcontroller should lookup the ip of the initial webapp,
-# overwrite the haproxy.cfg and restart haproxy.
-# I think this way we can best assure a good startup.
